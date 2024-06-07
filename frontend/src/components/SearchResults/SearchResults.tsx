e1c3fc154d77c148b1c31b8cb10120ff07b0b63a
Kelvin Kyendwa <kelvinkyendwa@gmail.com>
2024-06-07 15:51:34 +0300

    update styling of search results

--- frontend/src/components/SearchResults/SearchResults.tsx
+++ frontend/src/components/SearchResults/SearchResults.tsx
@@ -1,37 +1,51 @@
-import { Paper, List, ListItem } from "@mui/material";
+import {
+	Paper,
+	List,
+	ListItem,
+	ButtonBase,
+	Typography,
+	Box,
+} from "@mui/material";
 import { Book } from "../../types/types";
 
 interface Props {
 	searchItems: Book[];
-	setStudentList: React.Dispatch<React.SetStateAction<Book[]>>;
-	studentList: Book[];
+	addBookToReadingList: (book: Book) => void;
 }
-export const SearchResults = ({
-	searchItems,
-	setStudentList,
-	studentList,
-}: Props) => {
+export const SearchResults = ({ searchItems, addBookToReadingList }: Props) => {
 	return (
-		<Paper>
+		<Paper sx={{ height: "300px", overflowY: "scroll", zIndex: 50 }}>
 			<List>
 				{searchItems.map((book, index) => (
 					<ListItem key={index}>
-						{book.title}
-						<button
-							onClick={() => {
-								const updatedList = [...studentList, book];
-								const uniqueList = Array.from(
-									new Set(updatedList.map((book) => book.title)),
-								).map((title) =>
-									updatedList.find((book) => book.title === title),
-								);
-								setStudentList(
-									uniqueList.filter((book): book is Book => book !== undefined),
-								);
+						<Box
+							sx={{
+								display: "flex",
+								justifyContent: "space-between",
+								alignItems: "center",
+								padding: "10px",
+								width: "100%",
 							}}
 						>
-							Add
-						</button>
+							<img src={book.coverPhotoURL} alt={book.title} width={90} />
+							<Typography sx={{ width: "100%", paddingX: 5 }}>
+								{book.title}
+							</Typography>
+							<ButtonBase
+								onClick={() => addBookToReadingList(book)}
+								sx={{
+									backgroundColor: "#5ACCCC",
+									borderRadius: "15px",
+									height: "30px",
+									paddingX: "15px",
+									paddingY: "5px",
+									fontWeight: 600,
+									color: "#fff",
+								}}
+							>
+								Add
+							</ButtonBase>
+						</Box>
 					</ListItem>
 				))}
 			</List>
