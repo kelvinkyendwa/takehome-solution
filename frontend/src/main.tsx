51fbd8c987f56a6b0124783cc3f64b79b0293580
Kelvin Kyendwa <kelvinkyendwa@gmail.com>
2024-06-08 14:07:51 +0300

    add theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createTheme, ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const theme = createTheme({
+	typography: {
+		fontFamily: "Mulish, sans-serif",
+	},
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+);
